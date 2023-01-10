2a2eb0331e677c1fda9c46125979617469304697
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2023-01-09 21:42:42 -0300

    Esqueci de adicionar display none no menu novamente

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css', './mainMQ.component.css']
 })
+
 export class MainComponent {
 
 }
